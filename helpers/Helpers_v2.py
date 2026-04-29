e189b5bc33f67f735f265486bf1a208f1bf6eacb
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-04-29 08:36:46 +0000

    🔄 autosave @ 08:36:46

--- helpers/Helpers_v2.py
+++ helpers/Helpers_v2.py
@@ -47,7 +47,19 @@ import builtins
 import shutil
 import subprocess
 from pathlib import Path
+#
+from rich.console import Console
+from rich.markdown import Markdown
+import builtins
 
+console = Console()
+old_print = builtins.print
+
+def print(*args, sep=" ", end="\n", **kwargs):
+    text = sep.join(str(x) for x in args)
+    console.print(Markdown(text + end))
+
+#
 # ============================================================
 # PATHS — adjust if your folder structure differs
 # ============================================================
