1c5c2f3c24b2f0f5fbe3d3e8d29966dbacce385d
CaseyWongWc <147561210+CaseyWongWc@users.noreply.github.com>
2026-04-29 08:58:27 +0000

    🔄 autosave @ 08:58:27

---
+++ helpers/_workspaces/figure 13_1_3/solution.py
@@ -0,0 +1,82 @@
+class Item:
+    def __init__(self):
+        self.name = ""
+        self.quantity = 0
+
+    def set_name(self, nm):
+        self.name = nm
+
+    def set_quantity(self, qnty):
+        self.quantity = qnty
+
+    def display(self):
+        print(self.name, self.quantity)
+
+class Book(Item):
+    def __init__(self):
+        Item.__init__(self)
+        self.title = ""
+
+    def set_title(self, ttl):
+        self.title = ttl
+
+    def get_title(self):
+        return self.title
+
+class Textbook(Book):
+    def __init__(self):
+        Book.__init__(self)
+        self.edition = ""
+
+    def set_edition(self, edition):
+        self.edition = edition
+
+    def get_edition(self):
+        return self.edition
+
+class Audiobook(Book):
+    def __init__(self):
+        Book.__init__(self)
+        self.reader = ""
+
+    def set_reader(self, reader):
+        self.reader = reader
+
+    def get_reader(self):
+        return self.reader
+
+class Produce(Item):
+    def __init__(self):
+        Item.__init__(self)
+        self.expiration = ""
+
+    def set_expiration(self, expir):
+        self.expiration = expir
+
+    def get_expiration(self):
+        return self.expiration
+
+
+
+class Fruit(Produce):
+    def __init__(self):
+        Produce.__init__(self)
+        self.has_seeds = True
+
+    def set_has_seeds(self, sds):
+        self.has_seeds = sds
+
+    def get_has_seeds(self):
+        return self.has_seeds
+
+
+class Dairy(Produce):
+    def __init__(self):
+        Produce.__init__(self)
+        self.percent_fat = 3
+
+    def set_percent_fat(self, percent_fat):
+        self.percent_fat = percent_fat
+
+    def get_percent_fat(self):
+        return self.percent_fat
